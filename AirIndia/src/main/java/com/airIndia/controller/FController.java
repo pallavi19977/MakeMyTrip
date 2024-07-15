1218c427f69ae6394b7a52f3c932ca52b7f950fe
rames <rames@SHIVALIKA>
2024-07-15 12:31:51 +0530

    Airindia Updated 3rd Time-pallavi

--- AirIndia/src/main/java/com/airIndia/controller/FController.java
+++ AirIndia/src/main/java/com/airIndia/controller/FController.java
@@ -25,7 +25,7 @@ public class FController {
 	public String regSTData(@RequestBody Flight f)
 	{
 		Flight fl=fs.saveFData(f);
-		return " Flight Data Registered successfully.......!!";
+		return "AirIndia Flight Data Registered successfully.......!!";
 	}
 	@GetMapping("/getAllFData")
 	public List getFData()
